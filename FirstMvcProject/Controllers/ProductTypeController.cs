986643e7e6b7706890517bd37405ffd9dec724c4
kerimcalik <kcalik@gmail.com>
2021-12-05 10:19:47 +0300

    updated

---
+++ FirstMvcProject/Controllers/ProductTypeController.cs
@@ -0,0 +1,28 @@
+﻿using FirstMvcProject.Entity;
+using FirstMvcProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMvcProject.Controllers
+{
+    public class ProductTypeController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public ProductTypeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            //select * from ProductType
+            IEnumerable<ProductType> model = _db.ProductType;
+            
+            return View();
+        }
+    }
+}
